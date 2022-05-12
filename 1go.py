75237c5b6616937ffcae21a868c32aad5b9ac45a
Nick <70932969+Nicksalve@users.noreply.github.com>
2022-05-12 19:16:09 +0530

    Add files via upload

---
+++ 1go.py
@@ -0,0 +1,66 @@
+print("Type of nuber")
+
+a = 20
+print(type(a))
+
+b = 20.1
+print(type(b))
+
+print("Basic Operations on Numbers")
+
+a=2
+b=2.5
+print(a + b)
+print ((a + b) * 2)
+print(2 + 2 + 4 - 2/3)
+
+
+
+print("variables")
+
+first_name = "Joshua"
+last_name = "Bassett"
+age = 21
+is_adult = True
+
+print(first_name + last_name)
+print(age)
+
+
+
+print("taking an input")
+
+name = input("What is your name? ")
+print("Hello " + name)
+print("Nice to meet ya")
+
+
+print("sum of 2 numbers")
+first_number = input("Enter 1st number : ")
+second_number = input("Enter 2nd number : ")
+sum = float(first_number) + float(second_number)
+
+print("the sum is : " + str(sum))
+
+
+
+
+
+
+print("range")
+marks = [98,99,100]
+print (marks[1])
+
+
+
+
+print("Lists")
+
+friends = ["amar", "akbar", "anthony"]
+print(friends[0])
+print(friends[1])
+print(friends[-1])
+print(friends[-1])
+
+
+print("break & continue")
