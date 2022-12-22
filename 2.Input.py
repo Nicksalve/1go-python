c7570df2276be05ef5b2c95c3b62006e2ef18cf5
Nick <Ayush.salve2610@gmail.com>
2022-12-22 22:37:25 +0530

    Delete 2.Input.py

--- 2.Input.py
+++
@@ -1,9 +0,0 @@
-print("taking an input")
-
-'''
-here we are just taking an input from user 
-and then attaching a string with it.
-'''
-name = input("What is your name? ")
-print("Hello " + name)
-print("Nice to meet ya")
